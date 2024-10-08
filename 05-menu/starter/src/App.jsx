614ef7edaab85e4fbff05516a820f51daa3cb0e8
MXMaddux <maxmaddux@gmail.com>
2024-10-08 06:04:48 -0700

    Finish menu project

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -1,4 +1,38 @@
+import Categories from "./Categories.jsx";
+import Menu from "./Menu.jsx";
+import Title from "./Title";
+import menu from "./data.js";
+import { useState } from "react";
+
+const tempCategories = menu.map((item) => item.category);
+
+const tempSet = new Set(tempCategories);
+
+const tempItems = ["all", ...tempSet];
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
