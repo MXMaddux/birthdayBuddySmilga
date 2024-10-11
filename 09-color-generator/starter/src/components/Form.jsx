f6a82180aed08c959d7fbe65073a48a07ae354fc
MXMaddux <maxmaddux@gmail.com>
2024-10-11 05:12:21 -0700

    Finish color generator

---
+++ 09-color-generator/starter/src/components/Form.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const Form = ({ addColor }) => {
+  const [color, setColor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addColor(color);
+  };
+
+  return (
+    <section className="container">
+      <h4>color gen</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="#f15025"
+        />
+        <button type="submit" style={{ background: color }} className="btn">
+          submit
+        </button>
+      </form>
+    </section>
+  );
+};
+export default Form;
